42e36edae97e85cca0e2306209c325a4b2298c0c
Jonny Park <jnnypk@gmail.com>
2021-04-16 09:18:51 -0400

    delete gui version, combine two publisher into one, cui subsriver

--- Final/group_5_publisher.py
+++ Final/group_5_publisher.py
@@ -1,20 +1,29 @@
 import json
-import time, sys
+import time
+import sys
 import random
+import threading
 from datetime import datetime
 from typing import Dict
+from tkinter import *
 import paho.mqtt.client as mqtt
 import group_5_data_generator as dg
 
-class publisher:
+class Publisher:
     def __init__(self, start_id: int = None, max_msg: int = None) -> None:
-        self.start_id = start_id or 100
-        self.max_msg = max_msg or 500
+        self.start_id: int = start_id or 100
+        self.max_msg: int = max_msg or 500
         self.sample_set = dg.sample_set()
         # each publisher represents an IoT thermometer
         # each IoT thermometer has an unique serial number
-        # below code generates the unique S/N using time
-        self.serial = "SN"+ f"{round(random.random(), 12)}"[2:]
+        self.serial = "SN"+ f"{round(random.random(), 12)}"[2:] # generate a virtual serial number
+        # init gui
+        self.bar = Bar(self.serial)
+        # init mqttc messaging part in multi-thread
+        self.pub_thread = threading.Thread(target=self.send_data, daemon=True)
+        self.pub_thread.start()
+    
+    temperature: float = 0
 
     # get a single value from generator
     def get_data(self) -> float:
@@ -22,19 +31,18 @@ class publisher:
 
     # packaging the above value
     def create_data(self) -> Dict[str, str]:
-        # hosts = ["google.com", "yahoo.com", "facebook.com", "twitter.com"] # TODO I reckon this is useless
         self.start_id += 1
+        self.temperature = self.get_data() # get the temperature from the method
         payload = {
             "id": f"{self.start_id}",
             "time": f"{datetime.now()}",
-            "temperature": f"{self.get_data():.2f}",
+            "temperature": f"{self.temperature:.2f}",
             "unit": "celsius",
             "serial": self.serial
         }
         return payload
     
     # send data
-
     def send_data(self) -> None:
         mqttc = mqtt.Client()
         mqttc.on_connect = self._on_connect
@@ -61,6 +69,10 @@ class publisher:
             except (KeyboardInterrupt, SystemExit):
                 mqttc.disconnect()
                 sys.exit()
+        
+            # refresh GUI
+            self.bar.temperature = self.temperature
+            self.bar.refresh()
 
     # static methods for mqtt
 
@@ -77,4 +89,57 @@ class publisher:
 
     def _on_publish(self, mqttc, userdata, mid):
         print("Message published ID :{}".format(mid))
+    
+# GUI
+class Bar(Frame):
+    def __init__(self, serial: str) -> None:
+        super().__init__()
+        self.serial: str = serial
+        self.canvas = Canvas(self)
+        self.entry = Entry(self.canvas)
+        self.initUI()
+    
+    temperature: float = 0
+        
+    def initUI(self) -> None:
+        self.master.title('Thermometer')
+        self.pack(fill=BOTH, expand=1)
+        
+        # draw outline
+        self.canvas.create_rectangle(285, 20, 315, 980,outline='#222')
+        
+        # draw mark
+        temperatureList = ['50°C','40°C','30°C','20°C','10°C','0°C','10°C','20°C','30°C']
+        y = 100
+        for i in range(9):
+            self.canvas.create_line(320, y + 100 * i, 345, y + 100 * i,width=3)
+            self.canvas.create_text(350, 100 + 100 * i, anchor=W, font='Purisa', text=temperatureList[i])
+        for i in range(40):
+            self.canvas.create_line(320, y + 20 * i, 335, y + 20 * i)
+            
+        # draw mercury
+        y = 600 - abs(self.temperature)/80 * 800
+        self.mercury = self.canvas.create_rectangle(
+            290, y,                  #top left
+            310, 975,                #bottom right
+        outline='#222', fill='red')
+        
+        # prepare gui
+        self.canvas.create_text(20, 75, anchor=W, font='Purisa', text=self.serial)
+        self.canvas.create_text(20, 95, anchor=W, font='Purisa', text='Current Temperature:')
+        self.canvas.create_window(130,120,window=self.entry)
+        
+        # pack
+        self.canvas.pack(fill=BOTH, expand=1)
+        
+    # refresh command
+    def refresh(self):
+        self.entry.delete(0, 'end')
+        self.entry.insert(0,f"{self.temperature:.2f}")
+        self.canvas.coords(self.mercury, 290, 600 - abs(self.temperature)/80 * 800, 310, 975)
 
+# run the code
+root = Tk()
+root.geometry('450x750')
+pub = Publisher()
+root.mainloop()
--- Final/group_5_subscriber.py
+++ Final/group_5_subscriber.py
@@ -5,41 +5,55 @@ from datetime import datetime
 from typing import Dict
 import paho.mqtt.client as mqtt
 import group_5_data_generator as dg
-
+import threading
 
 class subscriber:
-    def __init__()
+    def __init__(self):
+        self.mqttc = mqtt.Client()
+        # init mqttc messaging part in multi-thread
+        self.msg_thread = threading.Thread(target=self.receive_data, daemon=True)
+        self.msg_thread.start()
+
+    def decode_msg(self, msg):
+        msg = msg.decode('utf-8')
+        payload = json.loads(msg)
+        print("\n----------- Decoded msg -----------\n")
+        self.print_data(payload)
+
+    def print_data(self, payload):
+        print(f"Message ID: {payload.get('id')}\n"\
+            f"Received Time: {payload.get('time')}\n"\
+            f"Temperature: {payload.get('temperature')}\n"\
+            f"Unit: {payload.get('unit')}\n"\
+            f"Serial Number: {payload.get('serial')}\n")
+
+    def receive_data(self) -> None:
+        self.mqttc.on_connect = self._on_connect
+        self.mqttc.on_disconnect = self._on_disconnect
+        self.mqttc.on_message = self._on_message
+        self.mqttc.on_subscribe = self._on_subscribe
+        self.mqttc.on_unsubscribe = self._on_unsubscribe
+        self.mqttc.connect(host='broker.emqx.io', port=1883)
+
+    def _on_connect(self, mqttc, userdata, flags, rc):
+        print('connected...rc=' + str(rc))
+        mqttc.subscribe(topic='iot/measure/thermometer', qos=0)
 
 
+    def _on_disconnect(self, mqttc, userdata, rc):
+        print('disconnected...rc=' + str(rc))
 
+    def _on_message(self, mqttc, userdata, msg):
+        print("Received message --------")
+        print('topic: ' + msg.topic + ', qos: ' + 
+            str(msg.qos) + ', message: ' + str(msg.payload))
+        self.decode_msg(msg.payload)
+    
+    def _on_subscribe(self, mqttc, userdata, mid, granted_qos):
+        print('subscribed (qos=' + str(granted_qos) + ')')
 
+    def _on_unsubscribe(self, mqttc, userdata, mid, granted_qos):
+        print('unsubscribed (qos=' + str(granted_qos) + ')')
 
-
-
-    def decode_msg(msg):
-    msg = msg.decode('utf-8')
-    payload = json.loads(msg)
-    print("\n----------- Decoded msg -----------\n")
-    #(payload)
-
-    def on_connect(mqttc, userdata, flags, rc):
-    print('connected...rc=' + str(rc))
-    mqttc.subscribe(topic='Final/#', qos=0)
-
-
-    def on_disconnect(mqttc, userdata, rc):
-    print('disconnected...rc=' + str(rc)
-
-
-    # Create Mqtt client
-    mqttc = mqtt.Client()
-    # Register callbacks
-    mqttc.on_connect = on_connect
-    mqttc.on_disconnect = on_disconnect
-    mqttc.on_message = on_message
-    mqttc.on_subscribe = on_subscribe
-    mqttc.on_unsubscribe = on_unsubscribe
-    # Connect to Mqtt broker on specified host and port
-    mqttc.connect(host='localhost', port=1883)
-    # Run Client forever
-    mqttc.loop_forever()
+sub1 = subscriber()
+sub1.mqttc.loop_forever()
